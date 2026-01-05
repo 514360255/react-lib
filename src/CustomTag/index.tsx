cac63ee1c8f5ad6662ffcf51f858bede92161078
guojiuling <514360255@qq.com>
2026-01-05 09:54:52 +0800

    fix: 1、custom tag 颜色取值

--- src/CustomTag/index.tsx
+++ src/CustomTag/index.tsx
@@ -3,6 +3,7 @@
  * @Date: 2025/11/11
  * @Description:
  */
+import { isEmptyValue } from '@guo514360255/antd-lib/utils/util';
 import { Tag } from 'antd';
 import React from 'react';
 
@@ -14,12 +15,12 @@ const CustomTag = (
   column?: any,
 ) => {
   if (column) {
-    const { color, text } = column.valueEnum[record.status] || {};
-    return <Tag color={color}>{text}</Tag>;
+    const { color, status, text } = column.valueEnum[record.status] || {};
+    return <Tag color={color || status}>{text}</Tag>;
   }
-  if (!value) return <>{value}</>;
-  const { color, text } = (valueEnum as any)[value] || {};
-  return <Tag color={color}>{text}</Tag>;
+  if (isEmptyValue(value)) return <>{value}</>;
+  const { color, status, text } = (valueEnum as any)[value] || {};
+  return <Tag color={color || status}>{text}</Tag>;
 };
 
 export default CustomTag;
